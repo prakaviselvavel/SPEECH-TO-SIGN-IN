e499621a3ea8daf92aaf45a06fde512457aef551
prakaviselvavel <prakavi30@gmail.com>
2025-05-01 20:15:47 +0100

    Initial commit

---
+++ base/views.py
@@ -0,0 +1,161 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import random
+import time
+from agora_token_builder import RtcTokenBuilder
+from .models import RoomMember
+import json
+from django.views.decorators.csrf import csrf_exempt
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import nltk
+nltk.download('punkt')
+from django.contrib.staticfiles import finders
+
+
+
+# Create your views here.
+
+def lobby(request):
+    return render(request, 'base/lobby.html')
+
+def room(request):
+    return render(request, 'base/room.html')
+
+
+def getToken(request):
+    appId = "ce0e070ac03f4922af4259eeb5c64fd2"
+    appCertificate = "10b26fc9a122472f960edd7a96a9ad84"
+    channelName = request.GET.get('channel')
+    uid = random.randint(1, 230)
+    expirationTimeInSeconds = 3600
+    currentTimeStamp = int(time.time())
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
+
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+
+    return JsonResponse({'token': token, 'uid': uid}, safe=False)
+
+
+@csrf_exempt
+def createMember(request):
+    data = json.loads(request.body)
+    member, created = RoomMember.objects.get_or_create(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name']
+    )
+
+    return JsonResponse({'name':data['name']}, safe=False)
+
+
+def getMember(request):
+    uid = request.GET.get('UID')
+    room_name = request.GET.get('room_name')
+
+    member = RoomMember.objects.get(
+        uid=uid,
+        room_name=room_name,
+    )
+    name = member.name
+    return JsonResponse({'name':member.name}, safe=False)
+
+@csrf_exempt
+def deleteMember(request):
+    data = json.loads(request.body)
+    try:
+        member = RoomMember.objects.get(
+            name=data['name'],
+            uid=data['UID'],
+            room_name=data['room_name']
+        )
+        member.delete()
+        return JsonResponse({'message': 'Member deleted successfully'}, safe=False)
+    except RoomMember.DoesNotExist:
+        return JsonResponse({'message': 'Member does not exist'}, status=404)
+
+@csrf_exempt
+def animation_view(request):
+	if request.method == 'POST':
+		text = request.POST.get('sen').lower()
+		#tokenizing the sentence
+		
+		#tokenizing the sentence
+		words = word_tokenize(text)
+
+		tagged = nltk.pos_tag(words)
+		tense = {}
+		tense["future"] = len([word for word in tagged if word[1] == "MD"])
+		tense["present"] = len([word for word in tagged if word[1] in ["VBP", "VBZ","VBG"]])
+		tense["past"] = len([word for word in tagged if word[1] in ["VBD", "VBN"]])
+		tense["present_continuous"] = len([word for word in tagged if word[1] in ["VBG"]])
+
+
+
+		#stopwords that will be removed
+		stop_words = set(["mightn't", 're', 'wasn', 'wouldn', 'be', 'has', 'that', 'does', 'shouldn', 'do', "you've",'off', 'for', "didn't", 'm', 'ain', 'haven', "weren't", 'are', "she's", "wasn't", 'its', "haven't", "wouldn't", 'don', 'weren', 's', "you'd", "don't", 'doesn', "hadn't", 'is', 'was', "that'll", "should've", 'a', 'then', 'the', 'mustn', 'i', 'nor', 'as', "it's", "needn't", 'd', 'am', 'have',  'hasn', 'o', "aren't", "you'll", "couldn't", "you're", "mustn't", 'didn', "doesn't", 'll', 'an', 'hadn', 'whom', 'y', "hasn't", 'itself', 'couldn', 'needn', "shan't", 'isn', 'been', 'such', 'shan', "shouldn't", 'aren', 'being', 'were', 'did', 'ma', 't', 'having', 'mightn', 've', "isn't", "won't"])
+
+
+
+		#removing stopwords and applying lemmatizing nlp process to words
+		lr = WordNetLemmatizer()
+		filtered_text = []
+		for w,p in zip(words,tagged):
+			if w not in stop_words:
+				if p[1]=='VBG' or p[1]=='VBD' or p[1]=='VBZ' or p[1]=='VBN' or p[1]=='NN':
+					filtered_text.append(lr.lemmatize(w,pos='v'))
+				elif p[1]=='JJ' or p[1]=='JJR' or p[1]=='JJS'or p[1]=='RBR' or p[1]=='RBS':
+					filtered_text.append(lr.lemmatize(w,pos='a'))
+
+				else:
+					filtered_text.append(lr.lemmatize(w))
+
+
+		#adding the specific word to specify tense
+		words = filtered_text
+		temp=[]
+		for w in words:
+			if w=='I':
+				temp.append('Me')
+			else:
+				temp.append(w)
+		words = temp
+		probable_tense = max(tense,key=tense.get)
+
+		if probable_tense == "past" and tense["past"]>=1:
+			temp = ["Before"]
+			temp = temp + words
+			words = temp
+		elif probable_tense == "future" and tense["future"]>=1:
+			if "Will" not in words:
+					temp = ["Will"]
+					temp = temp + words
+					words = temp
+			else:
+				pass
+		elif probable_tense == "present":
+			if tense["present_continuous"]>=1:
+				temp = ["Now"]
+				temp = temp + words
+				words = temp
+
+
+		filtered_text = []
+		for w in words:
+			path = "data/" + w.title() + ".mp4"
+			f = finders.find(path)
+			#splitting the word if its animation is not present in database
+			if not f:
+				for c in w:
+					filtered_text.append(c.upper())
+			#otherwise animation of word
+			else:
+				filtered_text.append(w.title())
+		words = filtered_text;
+
+
+		return JsonResponse({"text": text, "words": words})
+	else:
+		return render(request,'base/room.html')
